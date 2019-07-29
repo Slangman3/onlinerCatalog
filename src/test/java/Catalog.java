1c61d737f58b05d9f698f9204ac16ad69c1cf6f3
Slangman3 <slangman3@gmail.com>
2019-07-29 22:02:13 +0300

    Added catalog.java class.

---
+++ src/test/java/Catalog.java
@@ -0,0 +1,11 @@
+public class Catalog {
+    String name;
+    String description;
+    int minPrice;
+    int maxPrice;
+    String additional;
+    int date;
+    boolean beInUse;
+    boolean waterBox;
+
+}
